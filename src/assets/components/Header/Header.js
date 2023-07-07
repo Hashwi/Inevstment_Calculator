b3ac826f860c58ede7a785ca8094f5c7b1bab259
Hashwi <119130028+Hashwi@users.noreply.github.com>
2023-07-07 11:10:12 +0200

    Final App

--- src/assets/components/Header/Header.js
+++ src/assets/components/Header/Header.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import logo from '../../../assets/investment-calculator-logo.png';
+import classes from './Header.module.css';
 
-export default function Header() {
+const Header = () => {
   return (
-    <header className="header">
-    <img src={logo} alt="logo" />
-    <h1>Investment Calculator</h1>
-  </header>
-  )
-}
+    <header className={classes.header}>
+      <img src={logo} alt="logo" />
+      <h1>Investment Calculator</h1>
+    </header>
+  );
+};
+
+export default Header;
